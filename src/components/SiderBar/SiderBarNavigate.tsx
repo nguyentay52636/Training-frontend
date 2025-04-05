0b4e5e6595a6a9b49d4f34a887cbd0277bbfa487
Thienan1092005 <ogyminecraft497@gmail.com>
2025-04-05 20:02:57 +0700

    Update : loginform  UI

--- src/components/SiderBar/SiderBarNavigate.tsx
+++ src/components/SiderBar/SiderBarNavigate.tsx
@@ -1,15 +1,14 @@
-
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
 
 export default function SiderBarNavigate({ children }: { children: React.ReactNode }) {
-    return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                {children}
-            </main>
-        </SidebarProvider>
-    )
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 }
