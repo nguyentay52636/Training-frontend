d7269e08f369aae2de039a1c9f92877c08726b1f
tay52636 <phuongtay52636@gmail.com>
2025-05-17 19:06:32 +0700

    update form login

--- src/modules/Auth/pages/LoginPage.tsx
+++ src/modules/Auth/pages/LoginPage.tsx
@@ -17,15 +17,23 @@ export default function LoginPage() {
   }, [isAuthenticated, navigate]);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen p-6 bg-[url('https://thongtindaotao.sgu.edu.vn/assets/images/AQ1.png')] bg-no-repeat bg-right bg-cover"
-      style={{
-        backgroundSize: 'cover',
-        backgroundPosition: 'right',
-      }}
-    >
-      <div className='w-full flex justify-center'>
-        <div className=' backdrop-blur-md rounded-2xl bg-white p-4 md:p-8 bg-'>
+    <div className="flex min-h-screen">
+      {/* Bên trái: Hình ảnh + slogan */}
+      <div className="hidden md:flex flex-col justify-center items-start w-1/2 bg-gray-300 relative px-16">
+        <img
+          src="https://img.freepik.com/free-vector/abstract-soft-blue-watercolor-texture-design-background_1055-17547.jpg?semt=ais_hybrid&w=740"
+          className="absolute inset-0 w-full h-full object-cover opacity-80"
+        />
+        <div className=" flex flex-col items-center justify-center z-10 text-center">
+          <img src="/logosgu-removebg.png" alt="" className="text-white" />
+          <h1 className="text-4xl font-bold text-black">
+            Chào mừng bạn đến với quản lý của giảng viên SGU
+          </h1>
+        </div>
+      </div>
+      {/* Bên phải: Form login */}
+      <div className="flex flex-col justify-center items-center w-full md:w-1/2 bg-white">
+        <div className="w-full max-w-md">
           <LoginForm />
         </div>
       </div>
